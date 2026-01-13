c7cb10d277735939675b422190b48af258b1ec85
bjornpetersson1 <bjornpetersson1@live.se>
2026-01-13 16:10:17 +0100

    La till "add test data"-knappen

--- Labb2_WEWFY_Infrastructure/Data/Model/ExerciseLoggerEntityTypeConfig.cs
+++ Labb2_WEWFY_Infrastructure/Data/Model/ExerciseLoggerEntityTypeConfig.cs
@@ -13,119 +13,119 @@ public class ExerciseLoggerEntityTypeConfig : IEntityTypeConfiguration<ExerciseL
         builder.ToTable("ExerciseLoggers", "dbo");
 
         builder.HasData(
-            new ExerciseLogger
-            {
-                Id = 1,
-                Duration = new TimeSpan(0, 3, 0),
-                ExerciseId = 1,
-                WorkoutId = 1
-            },
-            new ExerciseLogger
-            {
-                Id = 2,
-                Duration = new TimeSpan(1, 40, 0),
-                ExerciseId = 4,
-                WorkoutId = 1
-            },
-            new ExerciseLogger
-            {
-                Id = 3,
-                Duration = new TimeSpan(0, 2, 0),
-                ExerciseId = 2,
-                WorkoutId = 1
-            },
-            new ExerciseLogger
-            {
-                Id = 4,
-                Duration = new TimeSpan(0, 2, 0),
-                ExerciseId = 1,
-                WorkoutId = 2
-            },
-            new ExerciseLogger
-            {
-                Id = 5,
-                Duration = new TimeSpan(0, 55, 0),
-                ExerciseId = 3,
-                WorkoutId = 2
-            },
-            new ExerciseLogger
-            {
-                Id = 6,
-                Duration = new TimeSpan(0, 5, 0),
-                ExerciseId = 2,
-                WorkoutId = 2
-            },
-            new ExerciseLogger
-            {
-                Id = 7,
-                Duration = new TimeSpan(0, 10, 0),
-                ExerciseId = 1,
-                WorkoutId = 3
-            },
-            new ExerciseLogger
-            {
-                Id = 8,
-                Duration = new TimeSpan(0, 50, 0),
-                ExerciseId = 3,
-                WorkoutId = 3
-            },
-            new ExerciseLogger
-            {
-                Id = 9,
-                Duration = new TimeSpan(0, 20, 0),
-                ExerciseId = 5,
-                WorkoutId = 3
-            },
-            new ExerciseLogger
-            {
-                Id = 10,
-                Duration = new TimeSpan(0, 5, 0),
-                ExerciseId = 1,
-                WorkoutId = 4
-            },
-            new ExerciseLogger
-            {
-                Id = 11,
-                Duration = new TimeSpan(1, 15, 0),
-                ExerciseId = 4,
-                WorkoutId = 4
-            },
-            new ExerciseLogger
-            {
-                Id = 12,
-                Duration = new TimeSpan(0, 1, 30),
-                ExerciseId = 2,
-                WorkoutId = 4
-            },
-            new ExerciseLogger
-            {
-                Id = 13,
-                Duration = new TimeSpan(0, 15, 0),
-                ExerciseId = 1,
-                WorkoutId = 5
-            },
-            new ExerciseLogger
-            {
-                Id = 14,
-                Duration = new TimeSpan(0, 50, 0),
-                ExerciseId = 5,
-                WorkoutId = 5
-            },
-            new ExerciseLogger
-            {
-                Id = 15,
-                Duration = new TimeSpan(0, 10, 0),
-                ExerciseId = 2,
-                WorkoutId = 5
-            },
-            new ExerciseLogger
-            {
-                Id = 16,
-                Duration = new TimeSpan(0, 35, 0),
-                ExerciseId = 5,
-                WorkoutId = 6
+            //new ExerciseLogger
+            //{
+            //    Id = 1,
+            //    Duration = new TimeSpan(0, 3, 0),
+            //    ExerciseId = 1,
+            //    WorkoutId = 1
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 2,
+            //    Duration = new TimeSpan(1, 40, 0),
+            //    ExerciseId = 4,
+            //    WorkoutId = 1
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 3,
+            //    Duration = new TimeSpan(0, 2, 0),
+            //    ExerciseId = 2,
+            //    WorkoutId = 1
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 4,
+            //    Duration = new TimeSpan(0, 2, 0),
+            //    ExerciseId = 1,
+            //    WorkoutId = 2
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 5,
+            //    Duration = new TimeSpan(0, 55, 0),
+            //    ExerciseId = 3,
+            //    WorkoutId = 2
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 6,
+            //    Duration = new TimeSpan(0, 5, 0),
+            //    ExerciseId = 2,
+            //    WorkoutId = 2
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 7,
+            //    Duration = new TimeSpan(0, 10, 0),
+            //    ExerciseId = 1,
+            //    WorkoutId = 3
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 8,
+            //    Duration = new TimeSpan(0, 50, 0),
+            //    ExerciseId = 3,
+            //    WorkoutId = 3
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 9,
+            //    Duration = new TimeSpan(0, 20, 0),
+            //    ExerciseId = 5,
+            //    WorkoutId = 3
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 10,
+            //    Duration = new TimeSpan(0, 5, 0),
+            //    ExerciseId = 1,
+            //    WorkoutId = 4
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 11,
+            //    Duration = new TimeSpan(1, 15, 0),
+            //    ExerciseId = 4,
+            //    WorkoutId = 4
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 12,
+            //    Duration = new TimeSpan(0, 1, 30),
+            //    ExerciseId = 2,
+            //    WorkoutId = 4
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 13,
+            //    Duration = new TimeSpan(0, 15, 0),
+            //    ExerciseId = 1,
+            //    WorkoutId = 5
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 14,
+            //    Duration = new TimeSpan(0, 50, 0),
+            //    ExerciseId = 5,
+            //    WorkoutId = 5
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 15,
+            //    Duration = new TimeSpan(0, 10, 0),
+            //    ExerciseId = 2,
+            //    WorkoutId = 5
+            //},
+            //new ExerciseLogger
+            //{
+            //    Id = 16,
+            //    Duration = new TimeSpan(0, 35, 0),
+            //    ExerciseId = 5,
+            //    WorkoutId = 6
 
-            }
+            //}
             );
     }
 }
--- Labb2_WEWFY_Presentation/ViewModels/MainWindowViewModel.cs
+++ Labb2_WEWFY_Presentation/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Labb2_WEWFY_Domain;
+using Labb2_WEWFY_Infrastructure.Data.Model;
+using Labb2_WEWFY_Presentation.Commands;
+using Labb2_WEWFY_Presentation.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using Labb2_WEWFY_Presentation.Commands;
-using Labb2_WEWFY_Presentation.Views;
 
 namespace Labb2_WEWFY_Presentation.ViewModels
 {
@@ -69,8 +71,9 @@ namespace Labb2_WEWFY_Presentation.ViewModels
             return IsTestdataAdded != true;
         }
 
-        private void AddTestData(object? obj)
+        private async void AddTestData(object? obj)
         {
+            await AddTestDataToDataBase();
             IsTestdataAdded = true;
         }
 
@@ -86,8 +89,178 @@ namespace Labb2_WEWFY_Presentation.ViewModels
                 CurrentView = view;
             }
         }
-        private async void AddTestDataToDataBase()
+        private async Task AddTestDataToDataBase()
         {
+            using var db = new WEWFYContext();
+
+            var testDataWorkouts = new List<Workout>
+            {
+                new Workout
+                {
+                    //Id = 1,
+                    Fueling = true,
+                    ExperienceRating = 3,
+                    WaterBefore = 330,
+                    WaterDuring = 0,
+                    LoggingDate = new DateTime(2025, 12, 01),
+                    Notes = "Spydde, annars bra"
+                },
+                new Workout
+                {
+                    //Id = 2,
+                    Fueling = false,
+                    ExperienceRating = 4,
+                    WaterBefore = 500,
+                    WaterDuring = 0,
+                    LoggingDate = new DateTime(2025, 12, 03),
+                    Notes = "Väldigt behagligt"
+                },
+                new Workout
+                {
+                    //Id = 3,
+                    Fueling = true,
+                    ExperienceRating = 5,
+                    WaterBefore = 400,
+                    WaterDuring = 500,
+                    LoggingDate = new DateTime(2025, 12, 06),
+                    Notes = "Lätt hela vägen"
+                },
+                new Workout
+                {
+                    //Id = 4,
+                    Fueling = false,
+                    ExperienceRating = 2,
+                    WaterBefore = 500,
+                    WaterDuring = 0,
+                    LoggingDate = new DateTime(2025, 12, 07),
+                    Notes = "Extremt tungt men avbröt inte"
+                },
+                new Workout
+                {
+                    //Id = 5,
+                    Fueling = true,
+                    ExperienceRating = 3,
+                    WaterBefore = 550,
+                    WaterDuring = 0,
+                    LoggingDate = new DateTime(2025, 12, 09),
+                    Notes = "Testade vingummi, kändes ok"
+                },
+                new Workout
+                {
+                    //Id = 6,
+                    Fueling = false,
+                    ExperienceRating = 3,
+                    WaterBefore = 300,
+                    WaterDuring = 0,
+                    LoggingDate = new DateTime(2025, 12, 11),
+                    Notes = "För kort, va stressad och blev inte trött"
+                }
+            };
+            await db.Workouts.AddRangeAsync(testDataWorkouts);
+            await db.SaveChangesAsync();
+
+            var testDataExerciseLoggers = new List<ExerciseLogger>
+            {
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 3, 0),
+                    ExerciseId = 1,
+                    WorkoutId = 1
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(1, 40, 0),
+                    ExerciseId = 4,
+                    WorkoutId = 1
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 2, 0),
+                    ExerciseId = 2,
+                    WorkoutId = 1
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 2, 0),
+                    ExerciseId = 1,
+                    WorkoutId = 2
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 55, 0),
+                    ExerciseId = 3,
+                    WorkoutId = 2
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 5, 0),
+                    ExerciseId = 2,
+                    WorkoutId = 2
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 10, 0),
+                    ExerciseId = 1,
+                    WorkoutId = 3
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 50, 0),
+                    ExerciseId = 3,
+                    WorkoutId = 3
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 20, 0),
+                    ExerciseId = 5,
+                    WorkoutId = 3
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 5, 0),
+                    ExerciseId = 1,
+                    WorkoutId = 4
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(1, 15, 0),
+                    ExerciseId = 4,
+                    WorkoutId = 4
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 1, 30),
+                    ExerciseId = 2,
+                    WorkoutId = 4
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 15, 0),
+                    ExerciseId = 1,
+                    WorkoutId = 5
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 50, 0),
+                    ExerciseId = 5,
+                    WorkoutId = 5
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 10, 0),
+                    ExerciseId = 2,
+                    WorkoutId = 5
+                },
+                new ExerciseLogger
+                {
+                    Duration = new TimeSpan(0, 35, 0),
+                    ExerciseId = 5,
+                    WorkoutId = 6
+
+                }
+            };
+            await db.ExerciseLoggers.AddRangeAsync(testDataExerciseLoggers);
+            await db.SaveChangesAsync();
 
         }
     }
