dc3041091f38c17a83c8bdf2def78dce95cbb928
bjornpetersson1 <bjornpetersson1@live.se>
2026-02-08 12:27:31 +0100

    Claryfication of what the filters in total stats do

--- Labb2_WEWFY_Presentation/ViewModels/InstructionsViewModel.cs
+++ Labb2_WEWFY_Presentation/ViewModels/InstructionsViewModel.cs
@@ -10,17 +10,16 @@ namespace Labb2_WEWFY_Presentation.ViewModels
     {
         public MainWindowViewModel MainVm { get; set; }
         public string Instructions { get; } =
-@"the idea:
+@"whatever works for you.
+
 track your runs based on your perceived effort and experience.
 by this you can make your own conclusion of whatever works for you.
 
+add test data, register runs, study and edit previous runs and browse your total stats with a number of filters.
 
-
-functions:
-add test data,
-register runs,
-study and edit previous runs
-and browse your total stats with a number of filters";
+type and rating filters include runs that match
+any selected option, while water and fueling filters 
+strictly exclude runs that do not meet the selected condition.";
         public InstructionsViewModel(MainWindowViewModel mainVm)
         {
             MainVm = mainVm;
