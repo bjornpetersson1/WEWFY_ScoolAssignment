7db44b7eb86d91e41b73c3faf71a9f304bcdb445
bjornpetersson1 <bjornpetersson1@live.se>
2026-02-08 13:54:37 +0100

    typo

--- Labb2_WEWFY_Presentation/ViewModels/InstructionsViewModel.cs
+++ Labb2_WEWFY_Presentation/ViewModels/InstructionsViewModel.cs
@@ -17,7 +17,7 @@ by this you can make your own conclusion of whatever works for you.
 
 add test data, register runs, study and edit previous runs and browse your total stats with a number of filters.
 
-type and rating filters include runs that match
+tempo and rating filters include runs that match
 any selected option, while water and fueling filters 
 strictly exclude runs that do not meet the selected condition.";
         public InstructionsViewModel(MainWindowViewModel mainVm)
